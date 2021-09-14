7c1a3ff61f6d818be880f32c7b4400b0b026b838
timfors <timfors@DESKTOP-2DKT2M1.localdomain>
2021-09-14 23:10:05 +0300

    asdasd

--- includes/philo.h
+++ includes/philo.h
@@ -6,6 +6,20 @@
 # include <pthread.h>
 # include <sus/time.h>
 
+typedef struct s_list_el
+{
+	void			*content;
+	struct s_list_el	*next;
+	struct s_list_el	*prev;
+}				t_list_el;
+
+typedef struct s_list
+{
+	int		size;
+	t_list_el	*start;
+	t_list_el	*end;
+}
+
 typedef struct s_logger
 {
 	void	*mut;
@@ -32,5 +46,10 @@ typedef struct s_philo
 	void	*fork_r;
 	void	*forl_l;
 }		t_philo
+
+t_list		*list_create(void *content);
+int		list_add(t_list *lst, void *content);
+void		list_clear(t_list **lst, void (*del)(void *));
+void		list_remove(t_list *lst, t_list_el el, void (*del)(void *));
 
 void	init_philo(int name, t_params params);
